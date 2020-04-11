ad84c7655142ef7fec72d71d0779eb37bfaee76f
gojavacoder <gojavacoder@gmail.com>
2020-04-11 16:15:22 +0530

    Added comments

--- src/com/coder/monagent/agentconstants/agentconstants.go
+++ src/com/coder/monagent/agentconstants/agentconstants.go
@@ -2,21 +2,33 @@ package agentconstants
 
 import (
 	"com/coder/initializer"
+
 	"github.com/chasex/glog"
 )
 
-const AGENT_NAME = "MonitoringAgent"
-const AGENT_CONF_FILE_NAME = "agent_conf.json"
-const LINUX_MONITORS_FILE_NAME = "linux_monitors.json"
+const (
+	// AgentName denotes the name of the agent
+	AgentName = "ServerMonitoringAgent"
+	// AgentConfFileName denotes the configuration file name of the agent
+	AgentConfFileName = "agent_conf.json"
+	// LinuxMonitorsFileName denotes the linux monitors file name of the agent
+	LinuxMonitorsFileName = "linux_monitors.json"
+)
 
+// Logger stores the instance of the logger
 var Logger *glog.Logger
+
+// ErrorLogger stores the instance of the error logger
 var ErrorLogger *glog.Logger
 
-var AGENT_CONF_FILE_PATH string
-var LINUX_MONITORS_FILE_PATH string
+// AgentConfFilePath denotes the configuration file path
+var AgentConfFilePath string
 
-func Initialize(){
-	AGENT_CONF_FILE_PATH = initializer.GetAgentConfDir() + "/" + AGENT_CONF_FILE_NAME
-	LINUX_MONITORS_FILE_PATH = initializer.GetAgentConfDir() + "/" + LINUX_MONITORS_FILE_NAME 
+// LinuxMonitorsFilePath denotes the linux monitors file path
+var LinuxMonitorsFilePath string
+
+// Initialize is used for setting agent constants.
+func Initialize() {
+	AgentConfFilePath = initializer.GetAgentConfDir() + "/" + AgentConfFileName
+	LinuxMonitorsFilePath = initializer.GetAgentConfDir() + "/" + LinuxMonitorsFileName
 }
-
--- src/com/coder/monagent/monagent.go
+++ src/com/coder/monagent/monagent.go
@@ -1,40 +1,42 @@
 package main
 
 import (
-	"com/coder/monagent/agentconstants"
-	"com/coder/logging"
 	"com/coder/initializer"
-	"com/coder/util"
-	"com/coder/shutdown"
+	"com/coder/logging"
+	"com/coder/monagent/agentconstants"
 	"com/coder/monagent/collector"
+	"com/coder/shutdown"
+	"com/coder/util"
 	"fmt"
 	"runtime"
 )
 
-func init(){
+// Initializes the server monitoring agent
+func init() {
 	fmt.Println("Init of MonAgent")
 	initializer.SetWorkingDirectories()
 	initLogging()
 	agentconstants.Initialize()
 	loadConfiguration()
-	runtime.GOMAXPROCS(3*runtime.NumCPU())
+	runtime.GOMAXPROCS(3 * runtime.NumCPU())
 	shutdown.GetShutdownHandler().Init(nil)
 }
 
-func initLogging(){
+//Initializes logging
+func initLogging() {
 	fmt.Println("=================== initLogging ============== ")
 	agentconstants.Logger = logging.GetLogger("agent", initializer.GetAgentLogDir(), true)
 	agentconstants.ErrorLogger = logging.GetLogger("error", initializer.GetAgentLogDir(), true)
 }
 
-func loadConfiguration(){
+// Loads the configuration needed for data collection by initializing the CollectorAPI
+func loadConfiguration() {
 	collector.GetcollectorAPI().Initialize()
 }
 
+// Entry point for the server monitoring agent
 func main() {
 	util.SetLimit()
 	collector.GetcollectorAPI().ScheduleDataCollection()
 	shutdown.Wait()
 }
-
-
