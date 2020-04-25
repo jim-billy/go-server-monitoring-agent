6aba5d8305b74fa1eaf312731e000d57adfee689
gojavacoder <gojavacoder@gmail.com>
2020-04-25 23:07:10 +0530

    changes

---
+++ cmd/monagent/monagent.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/gojavacoder/go-server-monitoring-agent/pkg/initializer"
+	"github.com/gojavacoder/go-server-monitoring-agent/pkg/logging"
+	"github.com/gojavacoder/go-server-monitoring-agent/pkg/monagent/agentconstants"
+	"github.com/gojavacoder/go-server-monitoring-agent/pkg/monagent/collector"
+	"github.com/gojavacoder/go-server-monitoring-agent/pkg/shutdown"
+
+	"github.com/gojavacoder/go-server-monitoring-agent/pkg/util"
+)
+
+// Initializes the server monitoring agent
+func init() {
+	fmt.Println("Init of MonAgent")
+	initializer.SetWorkingDirectories()
+	initLogging()
+	agentconstants.Initialize()
+	loadConfiguration()
+	runtime.GOMAXPROCS(3 * runtime.NumCPU())
+	shutdown.GetShutdownHandler().Init(nil)
+}
+
+//Initializes logging
+func initLogging() {
+	fmt.Println("=================== initLogging ============== ")
+	agentconstants.Logger = logging.GetLogger("agent", initializer.GetAgentLogDir(), true)
+	agentconstants.ErrorLogger = logging.GetLogger("error", initializer.GetAgentLogDir(), true)
+}
+
+// Loads the configuration needed for data collection by initializing the CollectorAPI
+func loadConfiguration() {
+	collector.GetcollectorAPI().Initialize()
+}
+
+// Entry point for the server monitoring agent
+func main() {
+	util.SetLimit()
+	collector.GetcollectorAPI().ScheduleDataCollection()
+	shutdown.Wait()
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/gojavacoder/go-server-monitoring-agent
+
+go 1.14
+
+require (
+	github.com/chasex/glog v0.0.0-20160217080310-c62392af379c
+	github.com/valyala/fasthttp v1.12.0
+)
