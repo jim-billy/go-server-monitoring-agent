404b2b412da000d2739f9b47adf1bf01ca5de1fb
gojavacoder <gojavacoder@gmail.com>
2020-05-01 20:34:44 +0530

    changes

---
+++ cmd/monagent/monagent_test.go
@@ -0,0 +1,42 @@
+package main
+
+import "testing"
+
+func Test_initLogging(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			initLogging()
+		})
+	}
+}
+
+func Test_loadConfiguration(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			loadConfiguration()
+		})
+	}
+}
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
+}
